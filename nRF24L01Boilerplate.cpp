9e5564d3825b059f985f201fadcdd99368e2fede
HamidPiri <hamidreza2623@gmail.com>
2026-02-01 15:11:00 +0330

    Working on reading status register from nrf24L01

--- nRF24L01Boilerplate.cpp
+++ nRF24L01Boilerplate.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/spi.h"
+#include <string.h>
 // SPI Defines
 // We are going to use SPI 0, and allocate it to the following GPIO pins
 // Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
@@ -12,7 +13,7 @@
 #define LED_PIN 25
 
 /***NRF24L01 Commands***/
-#define R_REGISTER 0B000AAAAA
+#define R_REGISTER 0B00000000
 #define W_REGISTER 0B001AAAAA
 #define R_RX_PAYLOAD 0B01100001
 #define W_TX_PAYLOAD 0B10100000
@@ -25,6 +26,47 @@
 #define W_TX_PAYLOAD_NO_ACK 0B10110000
 #define NOP 0B11111111
 /***NRF24L01 Commands***/
+
+/***Register Addresses***/
+
+#define STATUS 0x07
+#define CONFIG 0x00
+#define EN_AA 0x01
+#define EN_RXADDR 0x02
+#define SETUP_AW 0x03
+#define SETUP_RETR 0x04
+#define RF_CH 0x05
+#define RF_SETUP 0x06
+#define OBSERVE_TX 0x08
+#define RPD 0x09
+#define RX_ADDR_P0 0x0A
+#define RX_ADDR_P1 0x0B
+#define RX_ADDR_P2 0x0C
+#define RX_ADDR_P3 0x0D
+#define RX_ADDR_P4 0x0E
+#define RX_ADDR_P5 0x0F
+#define TX_ADDR 0x10
+#define RX_PW_P0 0x11
+#define RX_PW_P1 0x12
+#define RX_PW_P2 0x13
+#define RX_PW_P3 0x14
+#define RX_PW_P4 0x15
+#define RX_PW_P5 0x16
+#define FIFO_STATUS (1 << 5)
+/***Register Addresses***/
+
+static inline void csn_low() { gpio_put(PIN_CS, 0); }
+static inline void csn_high() { gpio_put(PIN_CS, 1); }
+
+uint8_t ReadRegisterValue(uint8_t regAddress, size_t length)
+{
+    static uint8_t buffer_Read[8];
+    memset(buffer_Read, 0, sizeof(buffer_Read));
+    csn_low();
+    spi_write_read_blocking(SPI_PORT, (uint8_t *)(R_REGISTER & regAddress), &buffer_Read[0], 1);
+    csn_high();
+    return buffer_Read[0];
+}
 
 int main()
 {
@@ -48,7 +90,6 @@ int main()
 
     while (true)
     {
-
 
         gpio_put(LED_PIN, 1);
         sleep_ms(500);
