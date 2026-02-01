09c2e8faf89b177f97c2d112c29f4dc9fc572c5a
HamidPiri <hamidreza2623@gmail.com>
2026-02-01 12:37:03 +0330

    Changed the board to pico

--- nRF24L01Boilerplate.cpp
+++ nRF24L01Boilerplate.cpp
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/spi.h"
-#include "pico/cyw43_arch.h"
-
 // SPI Defines
 // We are going to use SPI 0, and allocate it to the following GPIO pins
 // Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
@@ -11,6 +9,7 @@
 #define PIN_CS 17
 #define PIN_SCK 18
 #define PIN_MOSI 3
+#define LED_PIN 25
 
 /***NRF24L01 Commands***/
 #define R_REGISTER 0B000AAAAA
@@ -31,13 +30,6 @@ int main()
 {
     stdio_init_all();
 
-    // Initialise the Wi-Fi chip
-    if (cyw43_arch_init())
-    {
-        printf("Wi-Fi init failed\n");
-        return -1;
-    }
-
     // SPI initialisation. This example will use SPI at 1MHz.
     spi_init(SPI_PORT, 1000 * 1000);
     gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
@@ -45,20 +37,18 @@ int main()
     gpio_set_function(PIN_SCK, GPIO_FUNC_SPI);
     gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
 
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    gpio_put(LED_PIN, 1);
+
     // Chip select is active-low, so we'll initialise it to a driven-high state
     gpio_set_dir(PIN_CS, GPIO_OUT);
     gpio_put(PIN_CS, 1);
     // For more examples of SPI use see https://github.com/raspberrypi/pico-examples/tree/master/spi
 
-    // Example to turn on the Pico W LED
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
-    sleep_ms(2000);
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
-
     while (true)
     {
-        char key_input = getchar();
-        printf("Key pressed: %c\n", key_input);
-        key_input == 'A' ? cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1) : cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+        // char key_input = getchar();
+        // printf("Key pressed: %c\n", key_input);
+        // key_input == 'A' ? cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1) : cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
     }
 }
