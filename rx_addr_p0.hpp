310737d51cc637a4b81f91bf12827ba6de5dcdc0
HamidPiri <hamidreza2623@gmail.com>
2026-05-31 11:19:42 +0330

    rx_addr_p0 added

---
+++ rx_addr_p0.hpp
@@ -0,0 +1,16 @@
+#ifndef REGISTER_HPP
+#define REGISTER_HPP
+#include "register.hpp"
+#endif
+
+// Receive address data pipe 0. 5 Bytes maximum
+// length. (LSByte is written first. Write the number
+// of bytes defined by SETUP_AW)
+class rx_addr_p0 : public Register
+{
+public:
+    uint64_t RX_ADDR_P0;
+    rx_addr_p0() : Register(0x0A) {}
+
+private:
+};
