c6e98b5b1de2c8790d2e32d862f374520bfa6c98
HamidPiri <hamidreza2623@gmail.com>
2026-05-31 13:55:02 +0330

    TX_Address register added

---
+++ tx_addr.hpp
@@ -0,0 +1,22 @@
+#ifndef REGISTER_HPP
+#define REGISTER_HPP
+#include "register.hpp"
+#endif
+
+// Transmit address. Used for a PTX device only.
+// (LSByte is written first)
+// Set RX_ADDR_P0 equal to this address to han-
+// dle automatic acknowledge if this is a PTX
+// device with Enhanced ShockBurst™ enabled.
+class Tx_addr : public Register
+{
+public:
+    Tx_addr() : Register(0x0A) {}
+    bool SetTx_ADDR(uint64_t address = 0x10)
+    {
+        TX_ADDR = address;
+    }
+
+private:
+    uint64_t TX_ADDR;
+};
